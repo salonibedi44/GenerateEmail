595c3c167378720ebd968914479e59e32e56d9ab
unknown <salonibedi44@gmail.com>
2024-05-11 13:02:59 -0400

    Added to README and fixed the use of bio/calendar

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GenerateEmail
+
+Given a resume and interests the user had in this resume, this app generates an email requesting the owner of the resume for a coffee chat using Claude 3 Opus.
+
+About Claude 3 Opus:
+- free $5 worth of API credits for 14 days (compared to OpenAI $5/3 months)
+- Outperfoms SOTA industry models on 10 diverse benchmarks, with large margins (15%+) in coding, graduate reasoning, and math abilities
+- 200K context window
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,22 @@
 import anthropic
 import streamlit as st
-from pyresparser import ResumeParser
-from docx import Document
-import pandas as pd
 import PyPDF2
 
 st.title('Generate an email!')
 
 api_key = st.sidebar.text_input('Anthropic API key', type='password')
 
-def bio_submitted(bio, calendar):
+def bio_submitted():
     st.session_state.bio_submitted = True
-    st.session_state.bio = bio
-    st.session_state.calendar_link = calendar
+    st.session_state.bio = st.session_state.bio_key
+    st.session_state.calendar_link = st.session_state.calendar_key
+
+def clear_form():
+    st.session_state.interests = ""
+
 def generate_response(input_text):
     client = anthropic.Anthropic(api_key= api_key) 
-    message = client.messages.create(model="claude-3-opus-20240229", max_tokens=750, temperature=0.0, system = "Have a professional tone (for a business fraternity).", messages=[{"role":"user", "content": input_text}])
+    message = client.messages.create(model="claude-3-opus-20240229", max_tokens=750, temperature=0.0, system = "Have a professional tone. Assume that I don't know the person I am writing the email to.", messages=[{"role":"user", "content": input_text}])
     st.info(message.content[0].text)
 
 default_values = {'bio': '', 'calendar_link': '', 'bio_submitted': False,}
@@ -25,13 +26,13 @@ for key, value in default_values.items():
 
 if not st.session_state.bio_submitted:
     with st.form('first_page'): #binds multiple elements together, vals are sent in a batch
-        bio = st.text_area('Enter a bio of yourself that will be added to the beginning of the email.')
-        calendar = st.text_area('Enter your Calendly link')
-        st.form_submit_button('Submit', on_click = bio_submitted, args = (bio, calendar))      
+        bio = st.text_area('Enter a bio of yourself that will be added to the beginning of the email.', key = 'bio_key')
+        calendar = st.text_area('Enter your Calendly link', key = 'calendar_key')
+        st.form_submit_button('Submit', on_click = bio_submitted)      
            
 else:           
 #with st.form('my_form', border = True): #binds multiple elements together, vals are sent in a batch
-    interests = st.text_area('What are you interested in learning more about?')
+    interests = st.text_area('What are you interested in learning more about?', key = "interests")
     uploaded_file = st.file_uploader("Upload resume") 
     #submitted = st.form_submit_button('Submit') #required with st.form()
     
@@ -41,6 +42,7 @@ else:
         st.warning('Key is not correct!')
     if submitted and 'sk' in api_key:
         if not uploaded_file:
+            st.info(interests)
             st.warning('You have not uploaded a resume!')
         else:
             pdf_reader = PyPDF2.PdfReader(uploaded_file)
@@ -49,9 +51,9 @@ else:
             for page in range(len(pdf_reader.pages)):
                 content+=pdf_reader.pages[page].extract_text()
             #st.write(content)
-        
-            text = "Here is a bio of myself that is to be included in the beginning of the email you generate: " + st.session_state.bio + "] \n These are things I found interesting in the provided resume: [" + interests + "] \n Here is the resume: [" + content + "] \n. Now, generate a short email that details three things I would like to learn about from this resume (do not make it a bulleted list) in a coffee chat I would like to schedule this week or next using this provided link: " + st.session_state.calendar_link
+            
+            text = "Here is a bio of myself that is to be included in the beginning of the email you generate: " + st.session_state.bio + "] \n These are things I found interesting in the provided resume: [" + interests + "] \n Here is the resume: [" + content + "] \n. Now, generate a short email that details three things I would like to learn about from this resume (do not make it a bulleted list, and state why I would want to learn more about each of these things) in a coffee chat I would like to schedule this week or next using this provided link: " + st.session_state.calendar_link + ". Once again, MAKE SURE TO INCLUDE THE BIO AND CALENDAR LINK IN THE EMAIL!"
             generate_response(text)
 
-            again = st.button('Generate another email')
+            again = st.button('Generate another email', on_click = clear_form)
 
